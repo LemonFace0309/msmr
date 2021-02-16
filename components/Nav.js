47f5e12dd1fbbe9f65abcb2744b1db20cacfd43f
William Shi <33271663+williamsxz99@users.noreply.github.com>
2021-02-16 18:34:23 -0500

    Added nav items

--- components/Nav.js
+++ components/Nav.js
@@ -20,6 +20,8 @@ const MainNav = () => {
           <Nav.Link href="">About</Nav.Link>
           <Nav.Link href="">Resilience</Nav.Link>
           <Nav.Link href="">Research</Nav.Link>
+          <Nav.Link href="">Media</Nav.Link>
+          <Nav.Link href="">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
