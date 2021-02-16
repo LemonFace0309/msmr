90895c6c556bf12690aeaff249c1691858933a70
William Shi <33271663+williamsxz99@users.noreply.github.com>
2021-02-16 18:06:57 -0500

    Basic Navbar created

---
+++ components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import NavStyles from '../styles/Nav.module.css'
+import { Navbar, Nav } from 'react-bootstrap'
+
+const MainNav = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="">
+        <img
+          src="/logo.png"
+          width="23"
+          height="23"
+          className="d-inline-block align-baseline mx-2"
+        />
+        <span className={NavStyles.brand}>Multi-System Resilience</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="">Resilience</Nav.Link>
+          <Nav.Link href="">Research</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default MainNav
