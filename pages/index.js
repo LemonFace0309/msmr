a04249df7936622aed913c3dd7c4b84a48f4d8f9
Jimmy Yang <68363001+jimmyygit@users.noreply.github.com>
2021-02-17 18:49:30 -0500

    Closing #33; Title with underline component (#34)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Landing from '../components/Landing'
 import Jumbotron from '../components/Jumbotron'
+import Title from '../components/Title'
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
       <Landing />
       <Jumbotron />
       <h1> Hello World </h1>
+      <Title
+        title="There once was a bear and he ate a lot of fis"
+        position="middle"
+      />
     </>
   )
 }
