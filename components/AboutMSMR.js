44d9c9d5871ff0052a77afb1f21ea073d721c993
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-18 14:39:03 -0500

    Closing #7

--- components/AboutMSMR.js
+++ components/AboutMSMR.js
@@ -15,7 +15,7 @@ export default function AboutMSMR() {
           position="start"
         />
 
-        <p> The Multi-System Model of Resilience (MSMR) is a copyright-protected and empirically-validated, multi-dimensional model and measure of resilience capacity across different areas in your life </p>
+        <p> The Multi-System Model of Resilience (MSMR) is a copyright-protected and empirically-validated, multi-dimensional model and measure of resilience capacity across different areas in your life. </p>
         <p> The MSMR digital application provides an opportunity for users to engage and learn about their personal resilience capacity - where they stand, and how they can improve their resilience. </p>
 
         <Button type="purpleWhite" link="/">
---
+++ components/ResearchingResilience.js
@@ -0,0 +1,28 @@
+// @author Jimmy Yang
+import React from 'react'
+import Title from './Title'
+import Button from './Button'
+
+export default function ResearchingResilience() {
+  return (
+    <section className="container py-5 d-flex flex-column justify-content-center align-items-center">
+    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
+      <img className="order-0 order-md-1" width="50%" src="images/researchingres.jpeg" />
+
+      <div className="order-1 order-md-0 d-flex flex-column mx-3 py-2">
+        <Title
+          title="Researching Resilience"
+          position="start"
+        />
+
+        <p> MSMR conceptualizes resilience as a multi-dimensional capacity to respond to challenges across different domains. </p>
+        <p> Our team of researchers have conducted studiesto validate and examine the model in several publications. </p>
+
+        <Button type="purpleWhite" link="/">
+          <p>LEARN MORE</p>
+        </Button>
+      </div>
+    </div>
+  </section>
+  )
+}
