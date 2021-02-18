f669395a68d9da7e3f721ce5f0af087ffcf7df77
LemonFace0309 <charles.liu0309@gmail.com>
2021-02-18 16:30:17 -0500

    About (#41)
    * added banned component. closed #39
    
    * created using component for about page, responsive with medium width as breakpoint. closed #21
    
    * converted Using to shared component under nae TextAndPic, closing #44. Added documentation to shared component and Button component.
    
    * discovered dumb case sensitive error, preventing Vercel to deploy website >:(

---
+++ components/shared/Button.js
@@ -0,0 +1,32 @@
+// @author Charles Liu
+
+import Link from 'next/link'
+
+import styles from './Button.module.css'
+
+
+// type == 'purpleWhite' || type == 'whitePurple. Colour order reflects background colour -> text colour.
+// link is a url string to the target designation
+function Button({type, link, children}) {
+  let classes = [styles.container]
+
+  if (type == 'purpleWhite') {
+    classes.push(styles.purpleWhite)
+  } else if (type == 'whitePurple') {
+    classes.push(styles.whitePurple)
+  }
+  classes = classes.join(' ')
+  console.log(classes)
+
+  return (
+    <Link href={link}>
+      <a className={styles.link}>
+        <div className={classes}>
+          {children}
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default Button
