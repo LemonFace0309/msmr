9dc2e60327b02be68c26156a1ab6a4ae200caba5
Charles Liu <charles.liu0309@gmail.com>
2021-02-18 03:48:58 -0500

    created using component for about page, responsive with medium width as breakpoint. closed #21

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,18 @@
 /* @author Charles Liu */
 
 import Banner from '../components/shared/Banner'
+import Using from '../components/About/Using'
 
 function about() {
-    return (
-        <div>
-          <Banner url="/images/iphone_background_landscape.png" title="ABOUT MSMR" />
-          <h1>Hello World</h1>  
-        </div>
-    )
+  return (
+    <div>
+      <Banner
+        url="/images/iphone_background_landscape.png"
+        title="ABOUT MSMR"
+      />
+      <Using />
+    </div>
+  )
 }
 
 export default about
