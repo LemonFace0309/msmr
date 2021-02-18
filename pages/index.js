32c0a22dc49ecc4de9f8588b5181e1d905376a3a
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-17 19:55:56 -0500

    Closing #36: About MSMR

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,26 @@ export default function Home() {
     <>
       <Landing />
       <Jumbotron />
-      <h1> Hello World </h1>
+
+      <section className="container py-5 d-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
+          <img width="50%" src="images/aboutmsmr.png" />
+
+          <div className="d-flex flex-column ml-5 py-2">
+            <Title
+              title="About MSMR"
+              position="start"
+            />
+
+            <p> The Multi-System Model of Resilience (MSMR) is a copyright-protected and empirically-validated, multi-dimensional model and measure of resilience capacity across different areas in your life </p>
+            <p> The MSMR digital application provides an opportunity for users to engage and learn about their personal resilience capacity - where they stand, and how they can improve their resilience. </p>
+
+            <Button type="purpleWhite" link="/">
+              <p>LEARN MORE</p>
+            </Button>
+          </div>
+        </div>
+      </section>
 
 
       <section className="container py-5 d-flex flex-column justify-content-center align-items-center">
@@ -38,6 +57,16 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+
+
+
+
+
+
+
+
+      <h1> Hello World </h1>
     </>
   )
 }
