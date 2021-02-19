9b112716eb4965619bbf30ea1ad44d931df6b46c
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-19 14:23:45 -0500

    Made Nav heading clickable link

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import NavStyles from '../styles/Nav.module.css'
 import { Navbar, Nav } from 'react-bootstrap'
+import Link from 'next/link'
 
 const MainNav = () => {
   return (
@@ -14,7 +15,9 @@ const MainNav = () => {
           height="23"
           className="d-inline-block align-baseline mx-2"
         />
-        <span className={NavStyles.brand}>Multi-System Resilience</span>
+        <Link href="/">
+          <a className={NavStyles.Brand}> Multi-System Resilience </a>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
