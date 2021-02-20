1376cb3e2258767ddf446613ad0197d355871da7
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-19 19:14:41 -0500

    Closing #23

---
+++ pages/resilience.js
@@ -0,0 +1,24 @@
+// @author Jimmy Yang
+import Banner from '../components/shared/Banner'
+import TextAndPic from '../components/shared/TextAndPic'
+
+function resilience() {
+  return (
+    <div>
+      <Banner
+        url="/images/iphone_background_landscape.png"
+        title="RESILIENCE RESOURCES"
+      />
+      <TextAndPic textWidth={6} imageWidth={6} image="/images/resilience_resources.png">
+        <div>
+          <h2>Maximizing Your Resilience</h2>
+          <p className="text-justify">
+            There are multiple areas from which we draw our resilience. These include internal resilience, coping pursuits, and external resilience. Below, we provide an overview of how to maximize your resilience.
+          </p>
+        </div>
+      </TextAndPic>
+    </div>
+  )
+}
+
+export default resilience
