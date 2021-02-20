aade0b849cefb645d75179bde8ed676974902d0b
William Shi <33271663+williamsxz99@users.noreply.github.com>
2021-02-19 23:45:36 -0500

    Card (#46)
    * Create Card.js
    
    * changed learn more to read more
    
    * fixed card image sizing
    
    Added shared component documentation.
    
    -articles section and homepage take default props now
    
    Co-authored-by: LemonFace0309 <charles.liu0309@gmail.com>

---
+++ components/Articles.js
@@ -0,0 +1,25 @@
+// @author William Shi
+
+import React from 'react'
+import Card from './shared/Card'
+
+const Articles = ({ cards }) => {
+  const articles = Object.keys(cards).map((card, i) => (
+    <div className="m-3" key={i}>
+      <Card title={cards[card].title} img={cards[card].img}>
+        {cards[card].text}
+      </Card>
+    </div>
+  ))
+
+  return (
+    <>
+      <div className="text-center">
+        <h2 className="font-weight-bold">Articles</h2>
+      </div>
+      <div className="row mx-auto justify-content-center">{articles}</div>
+    </>
+  )
+}
+
+export default Articles
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Landing from '../components/Landing'
 import Jumbotron from '../components/Jumbotron'
 import MaxResilience from '../components/MaxResilience'
 import AboutMSMR from '../components/AboutMSMR'
+import Articles from '../components/Articles'
 import NewsAndMedia from '../components/NewsAndMedia'
 import ResearchingResilience from '../components/ResearchingResilience'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <>
       <Landing />
@@ -15,10 +16,30 @@ export default function Home() {
       <MaxResilience />
       <ResearchingResilience />
       <NewsAndMedia />
-
-
-
-      
+      <Articles cards={articles} />
     </>
   )
+}
+
+Home.defaultProps = {
+  articles: {
+    one: {
+      title: 'IPPA Leader Series Welcomes Dr. Jenny Liu',
+      img: '/images/aboutmsmr.png',
+      text:
+        'UPDATED: January 5, 2020 Dr. Jenny Liu delivers a presentation on resilience and how it can be strengthened...',
+    },
+    two: {
+      title: 'Resilience is More than your Mindset',
+      img: '/images/maxres.jpeg',
+      text:
+        'Wellness Solutions podcast hosts Mary Mangos and Danny Blackford explore the topic of resistance with Dr. Jenny Liu.',
+    },
+    three: {
+      title: 'What Does it Take to Increase Our Resilience?',
+      img: '/images/jumbotron.png',
+      text:
+        'Dr. Jenny Liu’s work is featured at the Society of Clinical Psychology. A small excerpt from her article is quoted below...',
+    },
+  },
 }
