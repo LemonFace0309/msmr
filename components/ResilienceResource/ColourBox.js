d89d4ba515bff988c4b7aff21b0e8283e8ee48b3
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-21 04:30:34 -0500

    Complete #24; Colourful Boxes

---
+++ components/ResilienceResource/ColourBox.js
@@ -0,0 +1,22 @@
+// @author Jimmy Yang
+import React from 'react'
+import styles from '../../styles/ColourBox.module.css'
+
+function ColourBox({ title, points, foreground, background, width, height, margin }) {
+  return (
+    <div
+      style={{ backgroundColor: background, width: width, height: height, margin: `5px ${margin}px` }}
+      className={styles.ColourBox}>
+      <h2 style={{ color: foreground }}> {title} </h2>
+      <ul>
+        {points.map(point => {
+          return (
+            <li> { point } </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default ColourBox
