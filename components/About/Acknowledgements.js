a0053cdc83f96b1c03c36ed908bf0ef5e9ba5928
LemonFace0309 <charles.liu0309@gmail.com>
2021-02-21 12:25:35 -0500

    Staff (#52)
    * refractored invalid props into valid ones and added key prop to Acknowledgements and Acknowledgement component
    
    * closed #22 added funders, staff researchers, and IT support
    
    * changed props in about.js from object to array
    
    * decreased size of funder images and centered text for better responsiveness in smaller viewports
    
    * made the font size smaller for the Button component

--- components/About/Acknowledgements.js
+++ components/About/Acknowledgements.js
@@ -6,23 +6,43 @@ import Col from 'react-bootstrap/Col'
 
 import styles from './Acknowledgements.module.css'
 import Acknowledgement from './Acknowledgement'
+import Funder from './Funder'
 
-function Acknowledgements({ title, people }) {
-  const recognition = Object.keys(people).map((person) => (
-    <Col md={2} className="mb-4">
-      <Acknowledgement
-        name={people[person].name}
-        titles={people[person].titles}
-      />
-    </Col>
-  ))
+function Acknowledgements({ title, subtitle, people, images }) {
+  let peopleRecognition
+  if (people) {
+    peopleRecognition = people.map((person) => (
+      <Col md={2} className="mb-4" key={person.name}>
+        <Acknowledgement
+          name={person.name}
+          titles={person.titles}
+        />
+      </Col>
+    ))
+  }
+
+  let imageRecognition
+  if (images) {
+    imageRecognition = images.map((img) => (
+      <Col md={4} className="mb-4" key={img.organization}>
+        <Funder url={img.url} caption={img.caption} />
+      </Col>
+    ))
+  }
 
   return (
     <Container fluid className={styles.container}>
       <Row className={styles.row}>
-        <h1 className={styles.title}>{title}</h1>
+        <h1>{title}</h1>
       </Row>
-      <Row className={styles.recogniationRow}>{recognition}</Row>
+      <Row className={styles.row}>
+        <h6 className={styles.subtitle}>
+          <em>{subtitle}</em>
+        </h6>
+      </Row>
+      <Row className={styles.recogniationRow}>
+        {peopleRecognition} {imageRecognition}
+      </Row>
     </Container>
   )
 }
