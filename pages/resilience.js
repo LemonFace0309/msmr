762b8d45362b59b2139c4e8856737074c8f659ab
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-21 01:13:23 -0500

    Enlarged image by putting padding

--- pages/resilience.js
+++ pages/resilience.js
@@ -10,7 +10,7 @@ function resilience() {
         title="RESILIENCE RESOURCES"
       />
       <TextAndPic textWidth={6} imageWidth={6} image="/images/resilience_resources.png">
-        <div>
+        <div style={{ padding: "200px 0"}}>
           <h2>Maximizing Your Resilience</h2>
           <p className="text-justify">
             There are multiple areas from which we draw our resilience. These include internal resilience, coping pursuits, and external resilience. Below, we provide an overview of how to maximize your resilience.
