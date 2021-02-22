02bdf564da0d76d3f0da8fc05b7c4a62c009cdf6
Jimmy Yang <jimmyy1337@gmail.com>
2021-02-22 01:27:05 -0500

    Minor documentations

--- components/ResilienceResource/ColourBox.js
+++ components/ResilienceResource/ColourBox.js
@@ -2,6 +2,17 @@
 import React from 'react'
 import styles from './ColourBox.module.css'
 
+/**
+ * ColourBox is a colourful box
+ * 
+ * @param {title} - title of box 
+ * @param {points} - array of bullet points 
+ * @param {foreground} - CSS value of foreground colour 
+ * @param {background} - CSS value of background colour 
+ * @param {width} - px width of each box 
+ * @param {height} - px height of each box 
+ * @param {margin} - px margin of each box 
+ */
 function ColourBox({ title, points, foreground, background, width, height, margin }) {
   return (
     <div
