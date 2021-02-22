26450c538eeee6ac47d12f435a7b5dba59ebb531
Jimmy Yang <68363001+jimmyygit@users.noreply.github.com>
2021-02-22 03:14:32 -0500

    Complete #24 Colourful Boxes (#59)
    * Created colourful title box component
    
    * Complete #24; Colourful Boxes
    
    * Created ColourBoxSection its own component
    
    * Moved files to be more modular
    
    * Minor documentations
    
    Co-authored-by: LemonFace0309 <charles.liu0309@gmail.com>

---
+++ components/ResilienceResource/ColourBox.js
@@ -0,0 +1,33 @@
+// @author Jimmy Yang
+import React from 'react'
+import styles from './ColourBox.module.css'
+
+/**
+ * ColourBox is a colourful box
+ * 
+ * @param {title} - title of box 
+ * @param {points} - array of bullet points 
+ * @param {foreground} - CSS value of foreground colour 
+ * @param {background} - CSS value of background colour 
+ * @param {width} - px width of each box 
+ * @param {height} - px height of each box 
+ * @param {margin} - px margin of each box 
+ */
+function ColourBox({ title, points, foreground, background, width, height, margin }) {
+  return (
+    <div
+      style={{ backgroundColor: background, width: width, height: height, margin: `5px ${margin}px` }}
+      className={styles.ColourBox}>
+      <h2 style={{ color: foreground }}> {title} </h2>
+      <ul>
+        {points.map(point => {
+          return (
+            <li> { point } </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default ColourBox
---
+++ components/ResilienceResource/ColourTitle.js
@@ -0,0 +1,19 @@
+// @author Jimmy Yang
+import React from 'react'
+import styles from './ColourTitle.module.css'
+
+// ColourTitle is a colourful boxed title
+// @param { title } - The title
+// @param { description } - The description for the title
+// @param { foreground } - CSS value colour for foreground
+// @param { background } - CSS value colour for background
+function ColourTitle({ title, description, foreground, background}) {
+  return (
+    <div className={styles.ColourTitle} style={{ backgroundColor: background }}>
+      <h1 style={{ color: foreground }}> { title } </h1>
+      <p style={{ color: foreground }}> { description } </p>
+    </div>
+  )
+}
+
+export default ColourTitle
