7014def18a1227e13448eee03132b7dfa9032a1f
OrenLeung <OrenLeung@users.noreply.github.com>
2021-02-25 18:25:57 +0000

    Apply formatting changes

--- components/Research/OngoingProjects.js
+++ components/Research/OngoingProjects.js
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-
 import styles from './OngoingProjects.module.css'
 import Card from '../shared/Card'
 
