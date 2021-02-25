a616c3f81e062ef82faee19c1f34ffa040d481b8
Oren Leung <47992694+OrenLeung@users.noreply.github.com>
2021-02-25 13:25:28 -0500

    Update OngoingProjects.js

--- components/Research/OngoingProjects.js
+++ components/Research/OngoingProjects.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+
 import styles from './OngoingProjects.module.css'
 import Card from '../shared/Card'
 
