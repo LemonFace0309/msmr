96567b7f480b2a064e9afa64f5361b8c25749077
jimmyygit <jimmyygit@users.noreply.github.com>
2021-03-04 17:44:23 +0000

    Apply formatting changes

--- components/MaxResilience.js
+++ components/MaxResilience.js
@@ -15,38 +15,36 @@ export default function MaxResilience() {
         MSMR offers the tool and knowledge to improve in each of these areas.{' '}
       </p>
 
-      <TextAndPic textWidth={6} imageWidth={6} image="/images/maxres.jpeg" isImageFirst={true}>
-          <h2 className="text-warning font-weight-bold">
-            {' '}
-            Internal Resilience{' '}
-          </h2>
-          <p>
-            {' '}
-            The ability to take care of yourself. This means identifying and
-            prioritizing mental health and physical health.{' '}
-          </p>
+      <TextAndPic
+        textWidth={6}
+        imageWidth={6}
+        image="/images/maxres.jpeg"
+        isImageFirst={true}
+      >
+        <h2 className="text-warning font-weight-bold"> Internal Resilience </h2>
+        <p>
+          {' '}
+          The ability to take care of yourself. This means identifying and
+          prioritizing mental health and physical health.{' '}
+        </p>
 
-          <h2 className="text-primary font-weight-bold"> Coping Pursuits </h2>
-          <p>
-            {' '}
-            The ability to address everyday challenges and goals. This means
-            identifying strengths, exploring and learning from new
-            opportunities.{' '}
-          </p>
+        <h2 className="text-primary font-weight-bold"> Coping Pursuits </h2>
+        <p>
+          {' '}
+          The ability to address everyday challenges and goals. This means
+          identifying strengths, exploring and learning from new opportunities.{' '}
+        </p>
 
-          <h2 className="text-success font-weight-bold">
-            {' '}
-            External Resilience{' '}
-          </h2>
-          <p>
-            {' '}
-            The ability to define your role within your community. Being active
-            in your community can help create your own identity.{' '}
-          </p>
+        <h2 className="text-success font-weight-bold"> External Resilience </h2>
+        <p>
+          {' '}
+          The ability to define your role within your community. Being active in
+          your community can help create your own identity.{' '}
+        </p>
 
-          <Button type="purpleWhite" link="/">
-            <p>LEARN MORE</p>
-          </Button>
+        <Button type="purpleWhite" link="/">
+          <p>LEARN MORE</p>
+        </Button>
       </TextAndPic>
     </section>
   )
