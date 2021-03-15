f048aaa5a1a5c80a6b3925fa4fc85b91610b4973
LemonFace0309 <charles.liu0309@gmail.com>
2021-03-15 02:32:15 -0400

    added new server onto next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['localhost', '35.182.254.224'],
+    domains: ['localhost', '35.182.254.224', '35.182.99.194', 'msmr-strapi-images.s3.ca-central-1.amazonaws.com'],
   },
 }
