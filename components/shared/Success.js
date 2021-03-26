c295d2fd59204d3a7fd842b869d001faa9b81013
LemonFace0309 <LemonFace0309@users.noreply.github.com>
2021-03-26 09:13:20 +0000

    Apply formatting changes

--- components/shared/Success.js
+++ components/shared/Success.js
@@ -6,7 +6,7 @@ import styles from './Success.module.css'
  * A success banner that should display after a user successfully submits something.
  * @param {React.Component} children - banner text
  */
-function Success({children}) {
+function Success({ children }) {
   return (
     <div className={styles.container}>
       <h5>{children}</h5>
