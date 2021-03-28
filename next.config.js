7ab9f36b460b7a4e5a4ffef3c3df947b707b162e
LemonFace0309 <charles.liu0309@gmail.com>
2021-03-28 05:09:28 -0400

    Articles (#94)
    * refractored invalid props into valid ones and added key prop to Acknowledgements and Acknowledgement component
    
    * closed #22 added funders, staff researchers, and IT support
    
    * changed props in about.js from object to array
    
    * decreased size of funder images and centered text for better responsiveness in smaller viewports
    
    * made the font size smaller for the Button component
    
    * install react reveal
    
    * created featured articles page with post components
    
    * added backend folder for strapi
    
    * created article category on backend and dynamically rendered them on featured-articles
    
    * created an article page that statically fetch props from their respective paths
    
    * created a dynamic article page
    
    * removed unncessary console logs
    
    * Apply formatting changes
    
    * connected to cloud backend
    
    * added new server onto next.config.js
    
    * Apply formatting changes
    
    * fixed dependency tree
    
    Co-authored-by: LemonFace0309 <LemonFace0309@users.noreply.github.com>

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+    domains: [
+      'localhost',
+      '35.182.254.224',
+      '35.182.99.194',
+      'msmr-strapi-images.s3.ca-central-1.amazonaws.com',
+    ],
+  },
+}
