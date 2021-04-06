73d2296e79925fa77701fa40f6e90dd0ea508d5f
Jimmy Yang <68363001+jimmyygit@users.noreply.github.com>
2021-04-05 20:37:30 -0400

    Footer (#117)
    * resolve #102
    
    * Resolve #105, #102, #42: Resp Footer
    
    * delete comments
    
    * Apply formatting changes
    
    * Target _blank ify
    
    * Apply formatting changes
    
    Co-authored-by: jimmyygit <jimmyygit@users.noreply.github.com>

--- components/Footer.js
+++ components/Footer.js
@@ -3,43 +3,46 @@ import styles from '../styles/Footer.module.css'
 import Link from 'next/link'
 
 const Footer = () => {
+  const date = new Date()
   return (
-    <footer className="row px-5 mx-5 py-5">
+    <footer className="row mx-5 py-5">
       <div className="container">
-        <div className="float-right">
-          <a href="https://twitter.com/resilient_u" target="_blank">
-            <img className={styles.icon} src="/icons/Twitter.png"></img>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/jenny-jw-liu-a7604846/"
-            target="_blank"
-          >
-            <img className={styles.icon} src="/icons/Linkedin.png"></img>
-          </a>
-          <a href="https://www.instagram.com/resilient.u/" target="_blank">
-            <img className={styles.icon} src="/icons/Instagram.png"></img>
-          </a>
-          <a href="https://www.facebook.com/RUResilient" target="_blank">
-            <img className={styles.icon} src="/icons/Facebook.png"></img>
-          </a>
+        <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
+          <p className="text-center">© 2019-{date.getFullYear()} MSMR Tool</p>
+          <div className={styles.Icons}>
+            <a target="_blank" href="https://twitter.com/resilient_u">
+              <i className="mx-2 fab fa-twitter"></i>
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/jenny-jw-liu-a7604846/"
+            >
+              <i className="mx-2 fab fa-linkedin-in"></i>
+            </a>
+            <a target="_blank" href="https://www.instagram.com/resilient.u/">
+              <i className="mx-2 fab fa-instagram"></i>
+            </a>
+            <a target="_blank" href="https://www.facebook.com/RUResilient">
+              <i className="mx-2 fab fa-facebook-f"></i>
+            </a>
+          </div>
         </div>
 
-        <p>© 2019-2021 MSMR Tool</p>
-        <a className="float-right" href="#">
-          back to top
-        </a>
-        <ul className="list-inline">
-          <li className="list-inline-item">
-            <Link href="/privacy">
-              <a>Privacy</a>
-            </Link>
-          </li>
-          <li className="list-inline-item">
-            <Link href="/terms">
-              <a>Terms</a>
-            </Link>
-          </li>
-        </ul>
+        <div className="my-3 d-flex flex-column flex-md-row justify-content-between align-items-center">
+          <ul className="list-inline text-center">
+            <li className="list-inline-item">
+              <Link href="/privacy">
+                <a>Privacy</a>
+              </Link>
+            </li>
+            <li className="list-inline-item">
+              <Link href="/terms">
+                <a>Terms</a>
+              </Link>
+            </li>
+          </ul>
+          <a href="#">back to top</a>
+        </div>
       </div>
     </footer>
   )
